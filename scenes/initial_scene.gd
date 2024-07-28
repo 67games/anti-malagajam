50f31642a88644502d9f635da903fcf34ed74daf
Sara Vidal <lostincase@gmail.com>
2024-07-28 08:24:08 +0200

    Improve initial scene

--- scenes/initial_scene.gd
+++ scenes/initial_scene.gd
@@ -3,7 +3,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("Fade in")
+	await await get_tree().create_timer(1).timeout
+	$AudioStreamPlayer.play()
 	await await get_tree().create_timer(3).timeout
+	$AnimationPlayer.play("Fade out")
+	await await get_tree().create_timer(2).timeout
+	$AnimationPlayer.play("Fade in")
+	$Sprite2D.hide()
+	await await get_tree().create_timer(4).timeout
 	GameManager.change_state(GameManager.States.START)
 	pass # Replace with function body.
 
